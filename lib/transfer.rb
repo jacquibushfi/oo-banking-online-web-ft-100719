f5ba127070e3b749ba1e4797cbc732693c33b5ed
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 19:06:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ attr_accessor :sender, :receiver, :amount, :status
   end
 
   def execute_transaction
-    if @sender.balance < @amount
+    if @sender.balance < @amount || valid?
        @status = "rejected"
        return "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
