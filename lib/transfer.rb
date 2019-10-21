dbd43b1a34462235df3cef28e64c201309604b33
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 19:19:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,5 +25,12 @@ attr_accessor :sender, :receiver, :amount, :status
       @status = "complete"
    end
   end
+def reverse_transer
+  if @status == "complete"
+    @sender.deposit(@amount) 
+    @receiver.deposit(@amount * -1)
+    @status = "reversed"
+  end
+end
 
 end
